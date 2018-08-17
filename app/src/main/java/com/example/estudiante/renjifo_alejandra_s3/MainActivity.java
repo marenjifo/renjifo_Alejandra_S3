fa5cc41ebd828dda5ad006be689ebe4005859af1
Estudiante <estudiante@SC311E22.local>
2018-08-17 14:32:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/estudiante/renjifo_alejandra_s3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.estudiante.renjifo_alejandra_s3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
